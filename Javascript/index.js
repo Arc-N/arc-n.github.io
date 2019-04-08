52d9f9d15d0b721e51d7e3cad61ef7faa423b150
Arman <49215886+Arc-N@users.noreply.github.com>
2019-04-08 11:45:13 -0700

    Add files via upload

---
+++ Javascript/index.js
@@ -0,0 +1,25 @@
+function navigationControl() {
+    if (document.getElementById("navigation").style.width != "180px") {
+        document.getElementById("navigation").style.width = "180px";
+        document.getElementById("openbutton").style.marginLeft = "140px";
+        document.getElementById("openbutton").style.color = "#999999";
+        document.getElementById("openbutton").style.transform = "rotate(45deg)";
+    } else {
+        document.getElementById("navigation").style.width = "0px";
+        document.getElementById("openbutton").style.marginLeft = "0px";
+        document.getElementById("openbutton").style.color = "#999999";
+        document.getElementById("openbutton").style.transform = "rotate(0deg)";
+    }
+}
+
+function hoverOn() {
+    document.getElementById("openbutton").style.color = "#ffffff";
+}
+
+function hoverOff() {
+    if (document.getElementById("openbutton").style.marginLeft === "180px") {
+        document.getElementById("openbutton").style.color = "#999999";
+    } else {
+        document.getElementById("openbutton").style.color = "#999999";
+    }
+}
